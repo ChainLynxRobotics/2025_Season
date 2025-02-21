aedd8f54cfcba8183f526ab2be78ba68cc352d77
TurtleMeds <turtle@turtle.contact>
2025-02-21 15:27:27 -0800

    moved DriveTests.java to commands folder and fixed imports and packages (it builds now!)

--- src/main/java/frc/robot/commands/DriveTests.java
+++ src/main/java/frc/robot/commands/DriveTests.java
@@ -1,17 +1,22 @@
-import frc.robot.util.TestCommand;
+package frc.robot.commands;
+
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.Drive.DriveModule;
+import frc.robot.util.TestCommand;
 
 public class DriveTests extends TestCommand {
 
-  private Drivetrain drivetrain;
+  private Drive drive;
   private DriveModule driveModule;
   private double speed;
   private double rotation;
   private double timeout;
   private Timer timer;
 
-  public DriveTests(Drivetrain drivetrain, DriveModule driveModule) {
-    this.drivetrain = drivetrain;
+  public DriveTests(Drive drive, DriveModule driveModule) {
+    this.drive = drive;
     this.driveModule = driveModule;
     timer = new Timer();
   }
@@ -27,7 +32,7 @@ public class DriveTests extends TestCommand {
 
   @Override
   public void execute() {
-    drivetrain.runModule(driveModule, speed, rotation);
+    drive.runModule(driveModule, speed, rotation);
   }
 
   @Override
