8efe842455d02968573d72da1959cc572ba75818
TurtleMeds <turtle@turtle.contact>
2025-02-21 15:27:27 -0800

    moved DriveTests.java to commands folder and fixed imports and packages (it builds now!)

--- src/main/java/frc/robot/util/TestCommand.java
+++ src/main/java/frc/robot/util/TestCommand.java
@@ -1,6 +1,11 @@
+package frc.robot.util;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+
 public class TestCommand extends Command {
   @Override
   public boolean isFinished() {
-    return !DriverStation.getInstance().isTest();
+    return !DriverStation.isTest();
   }
 }
