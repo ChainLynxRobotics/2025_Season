335bde8c6e9fa4728d4e1af4badcb0384c3b6c54
Rafferty <raffertymclain@gmail.com>
2025-02-22 11:14:43 -0800

    fix style

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,8 +20,6 @@ import static edu.wpi.first.units.Units.Newtons;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
 
-import java.util.Map;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.Matrix;
@@ -42,6 +40,7 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.LEDPattern.GradientType;
 import edu.wpi.first.wpilibj.util.Color;
+import java.util.Map;
 
 /**
  * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
@@ -119,44 +118,44 @@ public final class Constants {
   }
 
   public class LightConstants {
-  public static final int pwmPort = 1; // placeholder
-  public static final int numLeds = 60;
-  public static final Distance LED_SPACING = Meters.of(1.0 / numLeds); // check
+    public static final int pwmPort = 1; // placeholder
+    public static final int numLeds = 60;
+    public static final Distance LED_SPACING = Meters.of(1.0 / numLeds); // check
 
-  public enum LightsState {
-    IDLE,
-    HAS_CORAL,
-    DRIVING, // based on alliance color
-    ELEVATORING,
-    INTAKING,
-    STOPPED,
-    CELEBRATE
+    public enum LightsState {
+      IDLE,
+      HAS_CORAL,
+      DRIVING, // based on alliance color
+      ELEVATORING,
+      INTAKING,
+      STOPPED,
+      CELEBRATE
+    }
+
+    public static Map<LightsState, LEDPattern> ledMap =
+        Map.of(
+            LightsState.IDLE, LEDPattern.solid(Color.kWhite),
+            LightsState.HAS_CORAL, LEDPattern.solid(Color.kOrange),
+            LightsState.DRIVING,
+                LEDPattern.steps(
+                        Map.of(
+                            0,
+                            Color.kPurple,
+                            0.5,
+                            DriverStation.getAlliance().get() == Alliance.Blue
+                                ? Color.kBlue
+                                : Color.kRed))
+                    .scrollAtAbsoluteSpeed(MetersPerSecond.of(0.3), LED_SPACING),
+            LightsState.ELEVATORING,
+                LEDPattern.gradient(GradientType.kContinuous, Color.kRed, Color.kGreen)
+                    .mask(
+                        LEDPattern.progressMaskLayer(
+                            () -> 0.5)), // placeholder that should be elevator.getHeight() /
+            // elevator.maxHeight()
+            LightsState.INTAKING, LEDPattern.solid(Color.kPurple).breathe(Seconds.of(1)),
+            LightsState.STOPPED, LEDPattern.solid(Color.kDarkBlue),
+            LightsState.CELEBRATE,
+                LEDPattern.rainbow(255, 128)
+                    .scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LED_SPACING));
   }
-
-  public static Map<LightsState, LEDPattern> ledMap =
-      Map.of(
-          LightsState.IDLE, LEDPattern.solid(Color.kWhite),
-          LightsState.HAS_CORAL, LEDPattern.solid(Color.kOrange),
-          LightsState.DRIVING,
-              LEDPattern.steps(
-                      Map.of(
-                          0,
-                          Color.kPurple,
-                          0.5,
-                          DriverStation.getAlliance().get() == Alliance.Blue
-                              ? Color.kBlue
-                              : Color.kRed))
-                  .scrollAtAbsoluteSpeed(MetersPerSecond.of(0.3), LED_SPACING),
-          LightsState.ELEVATORING,
-              LEDPattern.gradient(GradientType.kContinuous, Color.kRed, Color.kGreen)
-                  .mask(
-                      LEDPattern.progressMaskLayer(
-                          () -> 0.5)), // placeholder that should be elevator.getHeight() /
-          // elevator.maxHeight()
-          LightsState.INTAKING, LEDPattern.solid(Color.kPurple).breathe(Seconds.of(1)),
-          LightsState.STOPPED, LEDPattern.solid(Color.kDarkBlue),
-          LightsState.CELEBRATE,
-              LEDPattern.rainbow(255, 128)
-                  .scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LED_SPACING));
-}
 }
